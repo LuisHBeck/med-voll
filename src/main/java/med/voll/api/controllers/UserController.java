5d087a3647e1688545f5a15c8060b0bb9cb0100c
LuisHBeck <beck.henrique.luis@gmail.com>
2023-11-14 08:08:55 -0300

    feat: user list

--- src/main/java/med/voll/api/controllers/UserController.java
+++ src/main/java/med/voll/api/controllers/UserController.java
@@ -7,11 +7,11 @@ import med.voll.api.domain.dtos.user.UserListData;
 import med.voll.api.domain.repositories.UserRepository;
 import med.voll.api.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
@@ -30,5 +30,17 @@ public class UserController {
         var user = userService.registerUser(data);
         var uri = uriBuilder.path("/user/{id}").buildAndExpand(user.getId()).toUri();
         return ResponseEntity.created(uri).body(new UserListData(user));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<UserListData>> listUsers(@PageableDefault(size = 10, sort = {"id"})Pageable pagination) {
+        var page = repository.findAll(pagination).map(UserListData::new);
+        return ResponseEntity.ok(page);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity listById(@PathVariable Long id) {
+        var user = repository.getReferenceById(id);
+        return ResponseEntity.ok(new UserListData(user));
     }
 }
